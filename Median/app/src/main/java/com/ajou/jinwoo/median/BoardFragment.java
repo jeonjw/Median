75d6b749d0f9d59518a4b76ab92f921d1f02f0e1
gch01410 <gch01410@naver.com>
2017-04-04 20:37:05 +0900

    board write ing..

--- Median/app/src/main/java/com/ajou/jinwoo/median/BoardFragment.java
+++ Median/app/src/main/java/com/ajou/jinwoo/median/BoardFragment.java
@@ -4,10 +4,14 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 /**
  * Created by samsung on 2017-03-31.
@@ -21,7 +25,7 @@ public class BoardFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_board,container,false);
-
+        setHasOptionsMenu(true);
         ((MainActivity) getActivity()).setToolbarTitle("Board");
 
         TabLayout tabLayout = (TabLayout) view.findViewById(R.id.board_tab);
@@ -57,5 +61,24 @@ public class BoardFragment extends Fragment{
 
 
         return view;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_notice_write) {
+            FragmentManager fm = getFragmentManager();
+            Fragment writeFragment = new BoardWriteFragment();
+            fm.beginTransaction().replace(R.id.student_notice_container, writeFragment).setTransition(android.support.v4.app.FragmentTransaction.TRANSIT_FRAGMENT_OPEN).addToBackStack(null).commit();
+        } else if (item.getItemId() == R.id.menu_search) {
+            Toast.makeText(getContext(), "board_search", Toast.LENGTH_SHORT).show();
+        }
+        return true;
+    }
+
+    @Override
+    public void onPrepareOptionsMenu(Menu menu) {
+        menu.findItem(R.id.menu_notice_write).setVisible(true);
+        menu.findItem(R.id.menu_search).setVisible(true);
     }
 }
