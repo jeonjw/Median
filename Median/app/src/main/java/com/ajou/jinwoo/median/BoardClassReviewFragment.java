92467fa7ff155814c79a7c8d94576c6054d15f7a
gch01410 <gch01410@naver.com>
2017-04-07 19:21:26 +0900

    수업후기 읽고 쓰기

--- Median/app/src/main/java/com/ajou/jinwoo/median/BoardClassReviewFragment.java
+++ Median/app/src/main/java/com/ajou/jinwoo/median/BoardClassReviewFragment.java
@@ -3,11 +3,18 @@ package com.ajou.jinwoo.median;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,46 +23,63 @@ import java.util.List;
 public class BoardClassReviewFragment extends Fragment {
 
     private RecyclerView mRecyclerView;
-    private ClassReviewAdapter classReviewAdapter;
+    private LectureReviewAdapter lectureReviewAdapter;
     private LectureReview mLectureReview;
-    private List<Integer> boardNumberList;
-    private List<String> titleList;
-    private List<Integer> commentNumberList;
+    private List<LectureReview> lectureReviewList;
+    private DatabaseReference mDatabase;
+
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_media_notice, container, false);
-        mRecyclerView = (RecyclerView) view.findViewById(R.id.board_class_review_recycler_view);
-//        mRecyclerView.setAdapter(classReviewAdapter);
 
-        boardNumberList = new ArrayList<>();
-        titleList = new ArrayList<>();
-        commentNumberList = new ArrayList<>();
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.board_lecture_review_recycler_view);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        lectureReviewList = new ArrayList<>();
+
+        loadNoticeData();
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        layoutManager.setReverseLayout(true);
+        layoutManager.setStackFromEnd(true);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        lectureReviewAdapter = new LectureReviewAdapter(lectureReviewList);
+        mRecyclerView.setAdapter(lectureReviewAdapter);
 
         return view;
     }
 
-    public void Data(){
+    private void loadNoticeData(){
+        mDatabase.child("수업후기").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    LectureReview lectureReview = ds.getValue(LectureReview.class);
+                    lectureReviewList.add(lectureReview);
+                }
+                lectureReviewAdapter.notifyDataSetChanged();
+            }
 
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
     }
 
-    private class ClassReviewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+    private class LectureReviewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        private TextView mBoardNumberTextView;
         private TextView mTitleTextView;
-        private TextView mCommentNumberTextView;
-        private boolean isClicked;
+        private TextView mContentsView;
 
-        public ClassReviewHolder(View itemView) {
+        public LectureReviewHolder(View itemView) {
             super(itemView);
 
-            mBoardNumberTextView = (TextView) itemView.findViewById(R.id.board_class_review_boardNumber_text_view);
-            mTitleTextView = (TextView) itemView.findViewById(R.id.board_class_review_title_text_view);
-            mCommentNumberTextView = (TextView) itemView.findViewById(R.id.board_class_review_comment_number_text_view);
+            mTitleTextView = (TextView) itemView.findViewById(R.id.board_lecture_review_title_text_view);
+            mContentsView = (TextView) itemView.findViewById(R.id.board_lecture_review_contents_text_view);
 
-            isClicked = false;
             itemView.setOnClickListener(this);
         }
 
@@ -66,29 +90,28 @@ public class BoardClassReviewFragment extends Fragment {
 
         public void bindNotice(LectureReview lectureReview) {
             mLectureReview = lectureReview;
-            mBoardNumberTextView.setText(mLectureReview.getBoardNumber());
             mTitleTextView.setText(mLectureReview.getTitle());
-            mCommentNumberTextView.setText(mLectureReview.getCommentNumber());
+            mContentsView.setText(mLectureReview.getContents());
         }
     }
 
-    private class ClassReviewAdapter extends RecyclerView.Adapter<ClassReviewHolder>{
+    private class LectureReviewAdapter extends RecyclerView.Adapter<LectureReviewHolder>{
         private List<LectureReview> mLectureReviewList;
 
-        public ClassReviewAdapter(List<LectureReview> mLectureReviewList) {
+        public LectureReviewAdapter(List<LectureReview> mLectureReviewList) {
             this.mLectureReviewList = mLectureReviewList;
         }
 
         @Override
-        public ClassReviewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public LectureReviewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
             final View view = layoutInflater.inflate(R.layout.list_item_lecture_review, parent, false);
 
-            return  new ClassReviewHolder(view);
+            return  new LectureReviewHolder(view);
         }
 
         @Override
-        public void onBindViewHolder(ClassReviewHolder holder, int position) {
+        public void onBindViewHolder(LectureReviewHolder holder, int position) {
             LectureReview lectureReview = mLectureReviewList.get(position);
             holder.bindNotice(lectureReview);
         }
