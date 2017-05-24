1e106b4c2e640453ef3762fc45fbe884786d8b5b
gch01410 <gch01410@naver.com>
2017-05-24 12:16:09 +0900

    알림설정 틀, 버전정보 다이얼로그, 개발자 네임카드

---
+++ Median/app/src/main/java/com/ajou/jinwoo/median/SettingAlarmOffButtonFragment.java
@@ -0,0 +1,21 @@
+package com.ajou.jinwoo.median;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by samsung on 2017-05-24.
+ */
+
+public class SettingAlarmOffButtonFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_setting_alarom_off, container, false);
+        return view;
+    }
+}
