6d74f8d00bb484cc19b246685925106e9687ce40
jeonjw <jeonjw1933@gmail.com>
2017-05-24 13:01:05 +0900

    알림 세팅화면 정리

--- Median/app/src/main/java/com/ajou/jinwoo/median/SettingAlarmButtonFragment.java
+++ Median/app/src/main/java/com/ajou/jinwoo/median/SettingAlarmButtonFragment.java
@@ -6,20 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 public class SettingAlarmButtonFragment extends Fragment {
 
-    private ImageView mediaOnToggle;
-    private ImageView mediaOffToggle;
-    private ImageView studentOnToggle;
-    private ImageView studentOffToggle;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_setting_alarm, container, false);
-
 
 
         return view;
